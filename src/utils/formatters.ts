103a0cbf09466c9ba58bf6e7ca469b3e98783730
deveroskp <kplee04@snu.ac.kr>
2025-12-08 02:04:44 +0900

    Feat: 백엔드 디렉토리 세팅

--- src/utils/formatters.ts
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (value: number) => {
-    return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(value);
-};
-
-export const formatNumber = (value: number) => {
-    return new Intl.NumberFormat('ko-KR').format(value);
-};
